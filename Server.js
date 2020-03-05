2b457a30d14b19bdc0767e084976610ba91bbc48
Farnaz Daneshvar <edfarnaz@gmail.com>
2020-03-05 15:23:54 -0700

    clean up, added username to request

--- Server.js
+++ Server.js
@@ -1,36 +1,40 @@
-var express = require('express')
-var app = express();
+const express = require('express')
+const app = express();
+const sql = require('mssql/msnodesqlv8')
 
-app.get('/', function(req, res){
-     var sql = require('mssql/msnodesqlv8')
-
-     // database config
-     var config ={
-        server: 'DESKTOP-4VC6QOA\\SQLEXPRESS',
-        database: 'advertiise-qa',
-        driver: 'msnodesqlv8',
+// database config
+let config ={
+    server: 'DESKTOP-4VC6QOA\\SQLEXPRESS',
+    database: 'advertiise-qa',
+    driver: 'msnodesqlv8',
     options : {
         trustedConnection : true
     }
-    }
+}
 
+app.get('/addProfiles', function(req, res){
      // connecting to database
      sql.connect(config, function(err){
-         if (err) console.log(err)
+        if (err) {
+            res.sendStatus(500);
+            return
+        }
         console.log("connection stablished")
-        var request = new sql.Request()
+        let request = new sql.Request()
+        let username = req.query.username
 
-        request.query("SELECT TOP 5  * FROM vwAdminAdProfiles" , function(err, recordset){
+        if (!username) {
+            res.status(400).send({message:"provider username"})      
+            return
+        }
+        request.query("SELECT TOP 5  * FROM vwAdminAdProfiles WHERE UserName = '"+username+"' " , function(err, recordset){
             if(err) console.log(err)
-
-            console.log(recordset.recordset)
             res.send(recordset.recordset)
         })
      })
-   
 })
 
 
-var server = app.listen(5000, function(){
+const server = app.listen(5000, function(){
     console.log('server is running.. ')
 })
